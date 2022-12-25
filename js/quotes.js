a78d3cbccd3c15b99a418f1a99375a1f1c9061dc
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2022-12-25 21:56:55 +0900

    221225 21:56 regular commit

--- js/quotes.js
+++ js/quotes.js
@@ -2,5 +2,48 @@ const quotes = [
     {
         quote : "quote1",
         author : "author1"
+    },
+    {
+        quote : "quote2",
+        author : "author2"
+    },
+    {
+        quote : "quote3",
+        author : "author3"
+    },
+    {
+        quote : "quote4",
+        author : "author4"
+    },
+    {
+        quote : "quote5",
+        author : "author5"
+    },
+    {
+        quote : "quote6",
+        author : "author6"
+    },
+    {
+        quote : "quote7",
+        author : "author7"
+    },
+    {
+        quote : "quote8",
+        author : "author8"
+    },
+    {
+        quote : "quote9",
+        author : "author9"
+    },
+    {
+        quote : "quote10",
+        author : "author10"
+    },
+    {
+        quote : "quote11",
+        author : "author11"
     }
 ]
+
+const randomQuote = document.querySelector("#randomQuote");
+randomQuote.innerText = quotes[Math.floor(Math.random() * 10)].author;
