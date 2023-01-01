c6380dfc1d846ff5a79353560cc3f166a6b75b4b
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-01-01 23:38:43 +0900

    230101 23:37 happy new year! 2023 daebak!!!

---
+++ 03_js/common.js
@@ -0,0 +1,11 @@
+/*
+function copy(){
+    var text = document.querySelector("#copyHere");
+    text.select();
+    document.execCommand('copy');
+    if(document.seletion){
+        document.selection.empty();
+    }else if(window.getSelection){
+        window.getSelection().removeAllRanges();
+    }
+}*/
