0e4d9f42b91a7ac1b519ebb3ddcaf75a2e049a29
thespeace <thespeace1@gmail.com>
2023-01-06 15:59:09 +0900

    230106 15:59 canvas start

---
+++ 03_js/canvas.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");//context는 그림을 그릴때 사용하는 붓(페인트 브러쉬)
+canvas.width = 800;
+canvas.height = 800;
+
+//ctx.fillRect(50, 50, 100, 200); //선을 그리고 rect, 채운다 fill / fillRect : 단축함수
+//ctx.rect();
+//ctx.fill();
