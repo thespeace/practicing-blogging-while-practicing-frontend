9fccada80b49981a63c0709aac097a808776714c
thespeace <thespeace1@gmail.com>
2023-01-08 18:13:47 +0900

    230108 regular commit

--- 03_js/canvas.js
+++ 03_js/canvas.js
@@ -1,11 +1,3 @@
-const canvas = document.querySelector("canvas");
-
-const ctx = canvas.getContext("2d");//context는 그림을 그릴때 사용하는 붓(페인트 브러쉬), 항상 좌표 0,0에서 시작한다.
-canvas.width = 700;
-canvas.height = 700;
-const lineWidth = document.querySelector("#line-width");
-const colorOptions = Array.from(document.querySelector(".color-option"));
-const color = document.querySelector("#color");
 
 /*fillRect()
 ctx.fillRect(50, 50, 100, 200); //rect로 선을 그리고, fill로 채운다. / fillRect : 단축함수 || shortcut function
@@ -39,6 +31,25 @@ ctx.arc(400, 400, 50,0,2 * Math.PI);
 ctx.fillStyle = 'white';
 ctx.fill();*/
 
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");//context는 그림을 그릴때 사용하는 붓(페인트 브러쉬), 항상 좌표 0,0에서 시작한다.
+
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 700;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-option")
+);
+const modeBtn = document.querySelector("#mode-btn");
+const destroyBtn = document.querySelector("#destroy-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+
+
 const colors = [
     "#ff3838",
     "#ffb8b8",
@@ -50,6 +61,7 @@ const colors = [
 ];
 
 let isPainting = false;
+let isFilling = false;
 const onMove= (event)=>{
     if(isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -75,16 +87,53 @@ const onColorChange=(event)=>{
 
 
 const onColorClick=(event)=>{
-    console.dir(event.target.dataset.color);
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
+const onModeClick=()=>{
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }else{
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+const onCanvasClick=()=>{
+    if(isFilling){
+        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    }else{
+
+    }
+}
+
+const onDestroyClick=()=>{
+    ctx.fillStyle= "rgb(37, 37, 37)";
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+
+const onEraserClick=()=>{
+    ctx.strokeStyle = "rgb(37, 37, 37)";
+    isFilling = false;
+    modeBtn.innerText = "Fill"
+    color.value = "rgb(37, 37, 37)";
 }
 
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
-
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click",onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
 
